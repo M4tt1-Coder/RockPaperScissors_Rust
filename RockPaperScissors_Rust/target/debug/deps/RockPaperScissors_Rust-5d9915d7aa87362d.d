9558ff906b499ac99f0b525ec85af219fab35064
M4tt1-Coder <geimat75@gmail.com>
2023-10-21 15:37:20 +0200

    removed double repo folder

--- RockPaperScissors_Rust/target/debug/deps/RockPaperScissors_Rust-5d9915d7aa87362d.d
+++
@@ -1,5 +0,0 @@
-/Users/m4tt1/programming/Rust/RockPaperScissors_Rust/RockPaperScissors_Rust/target/debug/deps/RockPaperScissors_Rust-5d9915d7aa87362d: src/main.rs
-
-/Users/m4tt1/programming/Rust/RockPaperScissors_Rust/RockPaperScissors_Rust/target/debug/deps/RockPaperScissors_Rust-5d9915d7aa87362d.d: src/main.rs
-
-src/main.rs:
